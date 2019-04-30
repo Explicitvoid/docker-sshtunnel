30ef14e7974a05fe5f73e94eaa82e6a2e270afe8
Explicitvoid <1491571+Explicitvoid@users.noreply.github.com>
2019-04-30 17:51:20 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ PASS=$(date +%s | sha256sum | base64 | head -c 32)
 adduser --disabled-password --gecos "" $USER
 echo "$USER:$PASS" | chpasswd
 echo -e "USER: \e[32m$USER\e[0m\nPASSWORD: \e[32m$PASS\e[0m"
-/usr/sbin/sshd -D
+/usr/sbin/sshd
+ssh -oStrictHostKeyChecking=no -R 0:localhost:22 serveo.net
