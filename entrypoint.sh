93ffa59f58c9a7143bc29b30cbd621dce84b5dde
Explicitvoid <1491571+Explicitvoid@users.noreply.github.com>
2019-04-30 19:41:28 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 USER=tunnel
 PASS=${PASS:=$(date +%s | sha256sum | base64 | head -c 32)}
-adduser --disabled-password --gecos "" $USER
+id -u $USER >/dev/null 2>&1 || adduser --disabled-password --gecos "" $USER
 echo "$USER:$PASS" | chpasswd
 echo -e "USER: \e[32m$USER\e[0m\nPASSWORD: \e[32m$PASS\e[0m"
 /usr/sbin/sshd -D
