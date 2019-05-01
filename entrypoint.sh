24135c38679a428b924faba522a2963a066dde48
Explicitvoid <1491571+Explicitvoid@users.noreply.github.com>
2019-04-30 20:24:56 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 USER=tunnel
 PASS=${PASS:=$(date +%s | sha256sum | base64 | head -c 32)}
 id -u $USER >/dev/null 2>&1 || adduser --disabled-password --gecos "" $USER
-echo "$USER:$PASS" | chpasswd
+echo "$USER:$PASS" | chpasswd >/dev/null 2>&1
 echo -e "USER: \e[32m$USER\e[0m\nPASSWORD: \e[32m$PASS\e[0m"
 
 if [ -n "$USESERVEO" ] && [ "$USESERVEO" == "yes" ] || [ "$USESERVEO" == "y" ] || [ "$USESERVEO" == "true" ]
